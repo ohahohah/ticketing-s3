85b1257b18769bdf9b98cd877d2417cb3d3e39de
SiyoungOh <dev@syoh.net>
2024-07-29 19:43:12 +0900

    Resolve conflict

---
+++ src/main/java/dev/syoh/ticketing3s/Ticketing3sApplication.java
@@ -0,0 +1,13 @@
+package dev.syoh.ticketing3s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ticketing3sApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ticketing3sApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/syoh/ticketing3s/Ticketing3sApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.syoh.ticketing3s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ticketing3sApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
